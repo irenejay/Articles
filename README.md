e204b4d81976da770ec8743b9a117d16c124ab5b
irene <irenejim9504@gmail.com>
2024-03-13 13:18:52 +0300

    Updated README file with more accurate and detailed information about the project, including usage examples and contributions guidelines

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Articles
+
+
+## Overview
+The Magazine Publisher System is a Python program designed to manage authors, magazines, and articles within a publishing environment. It allows users to create authors, magazines, and articles, associate articles with authors and magazines, and perform various operations such as retrieving information about articles, authors, and magazines.
---
+++ main.py
@@ -0,0 +1,102 @@
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise ValueError("Name must be of type str")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    def add_article(self, article):
+        if not isinstance(article, Article):
+            raise ValueError("Article must be of type Article")
+        self._articles.append(article)
+
+    def articles(self):
+        return self._articles
+
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+
+
+class Magazine:
+    _magazines = []
+    def __init__(self, name, category):
+        if not isinstance(name, str):
+            raise ValueError("Name must be of type str")
+        if len(name) < 2 or len(name) > 16:
+            raise ValueError("Name must be between 2 and 16 characters, inclusive")
+        if not isinstance(category, str):
+            raise ValueError("Category must be of type str")
+        if len(category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
+
+    def add_article(self, article):
+        if not isinstance(article, Article):
+            raise ValueError("Article must be of type Article")
+        self._articles.append(article)
+
+    def articles(self):
+        return self._articles
+
+    def contributors(self):
+        return list(set(article.author for article in self._articles))
+        return max(cls.magazines, key=lambda mag: len(mag.articles))
+
+
+
+
+class Article:
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise ValueError("Author must be an instance of Author")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be an instance of Magazine")
+        if not isinstance(title, str):
+            raise ValueError("Title must be of type str")
+        if len(title) < 5 or len(title) > 50:
+            raise ValueError("Title must be between 5 and 50 characters, inclusive")
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        author.add_article(self)
+        magazine.add_article(self)
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        self._author = author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, magazine):
+        self._magazine = magazine
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
