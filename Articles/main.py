f267a5621fd4a4aab981058e9f941fad7ebdc8a7
irene <irenejim9504@gmail.com>
2024-03-17 22:32:53 +0300

    Added Author class with initialization, properties, and methods as specified.

--- Articles/main.py
+++ Articles/main.py
@@ -2,35 +2,32 @@ from author import Author
 from article import Article
 from magazine import Magazine
 
-# testing 
-# author
+
 try: 
-  #create an author instance 
+  
   author1 = Author("Irene")
   print("Author name" , author1.name)
 
-  # test other methods 
+
 
 except ValueError as e:
     print("error author isntance " , e)
 
-# testing 
-# magazine
+
 try: 
-  #create an author instance 
+  
   magazine1 = Magazine("Tech Review", "Technology")
   magazine2 = Magazine("Science Today", "Science")
   print("Magazine name: " , magazine1.name)
   print("Magazine category: " , magazine1.category)
-  # test other methods 
+ 
 
 except ValueError as e:
     print("error author isntance " , e)
 
-# testing 
-# article
+
 try: 
-  #create an author instance 
+ 
   author2 = Author("Mary")
   article1 = Article(author1, magazine1, "The future of AI")
   article2 = Article(author1, magazine1, "The Daily Tech")
@@ -38,12 +35,12 @@ try:
   print("Atricle name: " , article1.title)
   print("Atricle author: " , article1.author.name)
   print("Atricle magazine: " , article1.magazine.name)
-  # test other methods 
+  
 
 except ValueError as e:
     print("error author instance " , e)
 
-# Testing additional methods and relationships
+
 try: 
   article4 = author1.add_article(magazine1, "AI Trends")
   article5 = author1.add_article(magazine2, "Science Breakthroughs")
@@ -53,8 +50,7 @@ try:
   for article in author1.articles():
     print("-" , article.title)
 
-    # magazine authors 
-    #  contributig authors 
+   
 except ValueError as e:
     print("error author isntance " , e)
 
